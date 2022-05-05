304c85b55e318a04925c8e1ea730777c61222121
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-05-05 16:44:28 -0400

    color play rgba

--- script.js
+++ script.js
@@ -23,28 +23,119 @@ function duplicate() {
 }
 
 // var cBoi = document.querySelector("#colorBoi");
-function changeColor(ele) {
-  var rgb_string = window
-    .getComputedStyle(ele, null)
-    .getPropertyValue("background-color");
-    
-  var rgb_arr = rgb_string.split(",");
-  rgb_arr[0] = parseInt(rgb_arr[0].substring(4, rgb_arr[0].length), 10);
-  rgb_arr[1] = parseInt(rgb_arr[1].substring(1, rgb_arr[1].length), 10);
-  rgb_arr[2] = parseInt(rgb_arr[2].substring(1, rgb_arr[2].length - 1), 10);
-  console.log(rgb_arr);
 
-  rgb_arr[0] = (rgb_arr[0] + 10) % 255;
+var addColor = 30;
+var addALpha = 0.2;
+function onMouseOverEventHandler(ele) {
+  var rgb_string;
+  try {
+    rgb_string = window
+      .getComputedStyle(ele, null)
+      .getPropertyValue("background-color");
+    // console.log(rgb_string);
+  } catch (error) {
+    rgb_string = "rgba(0, 0, 0, 0)";
+  }
+  var rgb_arr = rgb_string.split(",");
+  // console.log(rgb_arr);
+
+  rgb_arr[0] = parseInt(rgb_arr[0].substring(5, rgb_arr[0].length), 10);
+  rgb_arr[1] = parseInt(rgb_arr[1].substring(1, rgb_arr[1].length), 10);
+  rgb_arr[2] = parseInt(rgb_arr[2].substring(1, rgb_arr[2].length), 10);
+  rgb_arr[3] = parseFloat(rgb_arr[3].substring(1, rgb_arr[3].length - 1));
+  // console.log(rgb_arr);
+
+  // rgb_arr[0] = rgb_arr[0] + addColor > 255 ? 255 : rgb_arr[0] + addColor;
+  // rgb_arr[3] = rgb_arr[3] + addALpha > 0.99 ? 0.99 : rgb_arr[3] + addALpha;
+  switch (rgba_switch) {
+    case "r":
+      rgb_arr[0] = rgb_arr[0] + addColor > 255 ? 255 : rgb_arr[0] + addColor;
+      break;
+    case "g":
+      rgb_arr[1] = rgb_arr[1] + addColor > 255 ? 255 : rgb_arr[1] + addColor;
+      break;
+    case "b":
+      rgb_arr[2] = rgb_arr[2] + addColor > 255 ? 255 : rgb_arr[2] + addColor;
+      break;
+    default:
+      rgb_arr[3] = rgb_arr[3] + addALpha > 0.99 ? 0.99 : rgb_arr[3] + addALpha;
+  }
 
   var new_rgb_string =
-    "rgb(" + rgb_arr[0] + "," + rgb_arr[1] + "," + rgb_arr[2] + ")";
+    "rgba(" +
+    rgb_arr[0] +
+    "," +
+    rgb_arr[1] +
+    "," +
+    rgb_arr[2] +
+    "," +
+    rgb_arr[3] +
+    ")";
 
-  console.log(new_rgb_string);
+  // console.log(new_rgb_string);
 
   ele.style["background-color"] = new_rgb_string;
   // cBoi.style["background-color"] = new_rgb_string;
   // cBoi.style["background-color"] = "#aabbcc";
   // cBoi.style["background-color"] = "green";
+}
+function onClickEventHandler(ele) {
+  // console.log("HERE");
+  var rgb_string;
+  try {
+    rgb_string = window
+      .getComputedStyle(ele, null)
+      .getPropertyValue("background-color");
+    // console.log(rgb_string);
+  } catch (error) {
+    rgb_string = "rgba(0, 0, 0, 0)";
+  }
+  var rgb_arr = rgb_string.split(",");
+  // console.log(rgb_arr);
+
+  rgb_arr[0] = parseInt(rgb_arr[0].substring(5, rgb_arr[0].length), 10);
+  rgb_arr[1] = parseInt(rgb_arr[1].substring(1, rgb_arr[1].length), 10);
+  rgb_arr[2] = parseInt(rgb_arr[2].substring(1, rgb_arr[2].length), 10);
+  rgb_arr[3] = parseFloat(rgb_arr[3].substring(1, rgb_arr[3].length - 1));
+  // console.log(rgb_arr);
+
+  rgb_arr[0] = 0;
+  rgb_arr[1] = 0;
+  rgb_arr[2] = 0;
+  rgb_arr[3] = 0;
+
+  var new_rgb_string =
+    "rgba(" +
+    rgb_arr[0] +
+    "," +
+    rgb_arr[1] +
+    "," +
+    rgb_arr[2] +
+    "," +
+    rgb_arr[3] +
+    ")";
+
+  // console.log(new_rgb_string);
+
+  ele.style["background-color"] = new_rgb_string;
+  // cBoi.style["background-color"] = new_rgb_string;
+  // cBoi.style["background-color"] = "#aabbcc";
+  // cBoi.style["background-color"] = "green";
+}
+
+var rgba_switch = "a";
+
+function onClickR(ele) {
+  rgba_switch = "r";
+}
+function onClickG(ele) {
+  rgba_switch = "g";
+}
+function onClickB(ele) {
+  rgba_switch = "b";
+}
+function onClickA(ele) {
+  rgba_switch = "a";
 }
 
 function decimalToHexString(number) {
