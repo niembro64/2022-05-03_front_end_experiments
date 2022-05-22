57ef421a494767f12776bf462ec41ef2bfb3da6f
niemoBroIntegration <49417819+niemoBroIntegration@users.noreply.github.com>
2022-05-22 16:33:17 -0400

    less intense underline

--- script.js
+++ script.js
@@ -329,3 +329,27 @@ var e_array = [
 //   // original.parentNode.appendChild;
 //   // original.appendChild(clone);
 // }
+
+
+
+/////////////
+
+
+
+(function(){
+
+  var parallax = document.querySelectorAll("body"),
+      speed = 0.5;
+
+  window.onscroll = function(){
+    [].slice.call(parallax).forEach(function(el,i){
+
+      var windowYOffset = window.pageYOffset,
+          elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
+
+      el.style.backgroundPosition = elBackgrounPos;
+
+    });
+  };
+
+})();
